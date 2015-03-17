40aa972459bbc0e5d7dd3416f08913b5fc9c1843
hattuni <himoonjs@gmail.com>
2015-03-17 14:28:08 +0200

    new URL, view and template to display list

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -70,7 +70,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         #Fraincis starts a new list by entering a new item. He
         # is less interesting that Edith...
-        inputbox = self.brwoser.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
 
