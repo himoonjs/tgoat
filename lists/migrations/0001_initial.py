6cb58ac263d5dc67a85252258c944a43146a1ff5
hattuni <himoonjs@gmail.com>
2015-03-22 14:49:25 +0200

    Use Bootstrap to improve layout

--- lists/migrations/0001_initial.py
+++ lists/migrations/0001_initial.py
@@ -13,10 +13,26 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Item',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', auto_created=True, serialize=False, primary_key=True)),
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('text', models.TextField(default='')),
             ],
             options={
             },
             bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='List',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='item',
+            name='list',
+            field=models.ForeignKey(default=None, to='lists.List'),
+            preserve_default=True,
         ),
     ]
